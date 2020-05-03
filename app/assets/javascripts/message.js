9776909fc07caca6dcb14719e53b2ae839fd9fb8
kinopyyy <sss.namiki@gmail.com>
2020-05-03 14:25:34 +0900

    5/2 14:30LTGMお願いします.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,9 @@
 $(function(){
     function buildHTML(message){
       if ( message.image ) {
+        //data-idが反映されるようにしている
         var html =
-        `<div class="main_bar__chat__top_items">
+        `<div class="main_bar__chat__top_items" data-message-id=${message.id}>
           <div class="main_bar__chat__top_items__top">
             <div class="main_bar__chat__top_items__top__name">
               ${message.user_name}
@@ -20,8 +21,9 @@ $(function(){
         </div>`
       return html;
     } else {
+      //同様にdata-idが反映されるようにしている
       var html =
-      `<div class="main_bar__chat__top_items">
+      `<div class="main_bar__chat__top_items" data-message-id=${message.id}>
         <div class="main_bar__chat__top_items__top">
           <div class="main_bar__chat__top_items__top__nam">
             ${message.user_name}
@@ -42,7 +44,7 @@ $(function(){
 
 
 
-$('#new_message').on('submit', function(e){
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -54,14 +56,47 @@ $('#new_message').on('submit', function(e){
       processData: false,
       contentType: false
     })
-     .done(function(data){
-       var html = buildHTML(data);
-       $('.main_bar__chat').append(html);
-       $('.main_bar__chat').animate({ scrollTop: $('.main_bar__chat')[0].scrollHeight});
-       $('form')[0].reset();
-     })
-     .fail(function() {
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.main_bar__chat').append(html);
+        $('.main_bar__chat').animate({ scrollTop: $('.main_bar__chat')[0].scrollHeight});
+        $('form')[0].reset();
+      })
+      .fail(function() {
           alert("メッセージ送信に失敗しました");
-     })
-})
+      })
+  })
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.main_bar__chat:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/spi/messageとなるように文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.lenght !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身の１つ１つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.main_bar__chat').append(insertHTML);
+        $('.main_bar__chat').animate({ scrollTop: $('.main_bar__chat')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
