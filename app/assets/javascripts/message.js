acbe94f45c29c19976d8173d7e9102f70e52bb5c
kinopyyy <sss.namiki@gmail.com>
2020-05-04 17:56:14 +0900

    LTGMお願いします５.４.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,6 @@
 $(function(){
     function buildHTML(message){
       if ( message.image ) {
-        //data-idが反映されるようにしている
         var html =
         `<div class="main_bar__chat__top_items" data-message-id=${message.id}>
           <div class="main_bar__chat__top_items__top">
@@ -21,11 +20,10 @@ $(function(){
         </div>`
       return html;
     } else {
-      //同様にdata-idが反映されるようにしている
       var html =
       `<div class="main_bar__chat__top_items" data-message-id=${message.id}>
         <div class="main_bar__chat__top_items__top">
-          <div class="main_bar__chat__top_items__top__nam">
+          <div class="main_bar__chat__top_items__top__name">
             ${message.user_name}
           </div>
           <div class="main_bar__chat__top_items__top__date">
@@ -68,26 +66,20 @@ $(function(){
   })
 
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    var last_message_id = $('.main_bar__chat:last').data("message-id");
+    var last_message_id = $('.main_bar__chat__top_items:last').data("message-id");
+    
     $.ajax({
-      //ルーティングで設定した通り/groups/id番号/spi/messageとなるように文字列を書く
       url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
       if (messages.lenght !== 0) {
-        //追加するHTMLの入れ物を作る
         var insertHTML = '';
-        //配列messagesの中身の１つ１つを取り出し、HTMLに変換したものを入れ物に足し合わせる
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
         });
-        //メッセージが入ったHTMLに、入れ物ごと追加
         $('.main_bar__chat').append(insertHTML);
         $('.main_bar__chat').animate({ scrollTop: $('.main_bar__chat')[0].scrollHeight});
       }
